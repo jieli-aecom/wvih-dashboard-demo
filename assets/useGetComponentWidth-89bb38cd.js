211843a22c18ffd663e27941f9aef34790a5fdbd
Jie Li <jie.li@aecom.com>
2025-07-11 10:24:40 -0400

    init pages

---
+++ assets/useGetComponentWidth-89bb38cd.js
@@ -0,0 +1,1 @@
+import{r}from"./index-c785dd4a.js";function o(){const e=r.useRef(null),[n,s]=r.useState(0);return r.useEffect(()=>{const t=()=>{e.current&&s(e.current.offsetWidth)};return window.addEventListener("resize",t),t(),()=>{window.removeEventListener("resize",t)}},[e]),{ref:e,width:n}}export{o as u};
